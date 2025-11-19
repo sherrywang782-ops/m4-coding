1f39dd7ab83187eb0d59269c4331dd7103774915
14386 <sherrywang782@gmail.com>
2025-11-19 16:35:56 -0500

    Task 1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -5,11 +5,23 @@ package theater;
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
+        this.setAudience(audience);
+    }
+
+    public int getAudience() {
+        return audience;
+    }
+
+    public void setAudience(int audience) {
         this.audience = audience;
+    }
+
+    public String getPlayID() {
+        return playID;
     }
 }
